4176f17f4298aba7410b7600b8b4759313719919
Alan93 <52204680+wangnan9394@users.noreply.github.com>
2021-01-03 11:41:08 +0800

    Update Sniffles_vcf_2_vg_vcf.py

--- Sniffles_vcf_2_vg_vcf.py
+++ Sniffles_vcf_2_vg_vcf.py
@@ -2,30 +2,35 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description = 'input ngmlr_vcf, output vg validation vcf', add_help = False, \
-    usage = ' -i [input.vcf] -g [genome name] ')
+    usage = ' -i [input.vcf] -g [genome name] -p [process] -t [tmp]：-p all,normal,sort,check, default=all>')
 required = parser.add_argument_group()
 optional = parser.add_argument_group()
 required.add_argument('-i', '--input', metavar = '[input.vcf]', help = 'vcf file', required = True)
 required.add_argument('-g', '--genome', metavar = '[genome name]', help = 'genome name', required = True)
-
+required.add_argument('-p', '--process', metavar = '[process]', help = 'start from the stage', default='all',type=str)
+required.add_argument('-t', '--tmp', metavar = '[tmp dir]', help = 'make a tmp dir', default='0',type=str)
 args = parser.parse_args()
 
 genome=args.genome
 input=args.input
+process=['all','normal','sort','clear']
+if args.tmp=='1':
+    cmd_mkdir='mkdir {}'.format(input[:-4])
+    cmd_cp1='cp {} {}'.format(genome,input[:-4])
+    cmd_cp2='cp {} {}'.format(input,input[:-4])
+    cmd_cd='cd {}'.format(input[:-4])
+    os.system(cmd_mkdir)
+    os.system(cmd_cp1)
+    os.system(cmd_cp2)
+    os.system(cmd_cd)
+    os.chdir(input[:-4])
+    os.getcwd()
 
-cmd_mkdir='mkdir {}'.format(input[:-4])
-cmd_cp1='cp {} {}'.format(genome,input[:-4])
-cmd_cp2='cp {} {}'.format(input,input[:-4])
-cmd_cd='cd {}'.format(input[:-4])
-os.system(cmd_mkdir)
-os.system(cmd_cp1)
-os.system(cmd_cp2)
-os.system(cmd_cd)
-os.chdir(input[:-4])
-os.getcwd()
+if args.tmp=='0':
+    os.getcwd()
 
-print("normal start!!")
 def ngmlr_vcf_2_normal_vcf(originalfile):
+    print("normal start!!")
     #cmd_pre1 = 'rm DB02_ngmlr.new.vcf'
     #cmd_pre2 = 'rm tmp1.txt'
     #cmd_pre3 = 'rm tmp2.txt'
@@ -33,7 +38,7 @@ def ngmlr_vcf_2_normal_vcf(originalfile):
     #os.system(cmd_pre2)
     #os.system(cmd_pre3)
     outfilename=originalfile[:-4]+'.normal.vcf'
-    outfile=open(outfilename,'a')
+    outfile=open(outfilename,'w')
     with open(originalfile,'r') as f:
         ID=0
         for line in f:
@@ -101,12 +106,45 @@ def ngmlr_vcf_2_normal_vcf(originalfile):
                                         ll+=str(i)+'\t'
                                     ll1=ll+'\n'
                                     outfile.write(ll1)
-##run step1
-ngmlr_vcf_2_normal_vcf(input)
 
 
-print("clear start!!")
+
+def sort_vcf(filename):
+    print("sort start!!")
+    new_filename = filename[:-4]+'.sort.vcf'
+    cmd_s1='chr_order="chrMnchr1nchr2nchr3nchr4nchr5nchr6nchr7nchr8nchr9nchrUn"'
+    os.system(cmd_s1)
+    cmd_s2='cat {} | grep "^#" > header.vcf'.format(filename)
+    os.system(cmd_s2)
+    cmd_s3='cat {} | grep -v "^#" | sort -k1,1 -k2,2n > pre.sorted.vcf'.format(filename)
+    os.system(cmd_s3)
+    cmd_s4='cat header.vcf pre.sorted.vcf >{}'.format(new_filename)
+    os.system(cmd_s4)
+
+
+
+def reduce_vcf(filename):
+    print("reduce start!!")
+    readDir = filename
+    writeDir = filename[:-4]+'.reduce.vcf'
+    outfile=open(writeDir,"w")
+    f = open(readDir,"r")
+    pos_seen = set()  # Build an unordered collection of unique elements.
+    for line in f:
+        line = line.strip('\n')
+        line2=line.split('\t')
+        if len(line2)==1:
+            outfile.write(line +'\n')
+        if len(line2)>1:
+            pos = line2[1]
+            if pos not in pos_seen:
+                outfile.write(line+ '\n')
+                pos_seen.add(pos)               
+
+
+
 def clear_vcf(filename,genome):
+    print("clear start!!")
     for i in range(100):
         #print("round",str(i+1))
         cmd_0 = 'vg construct -r {} -v {} 1>test.vg 2>error'.format(genome,filename)
@@ -126,55 +164,56 @@ def clear_vcf(filename,genome):
         os.system(cmd_1)
         if error_position.isnumeric()==False:
             break
-##run step2
-clear_vcf(input[:-4]+'.normal.vcf',genome)
 
 
-print("sort start!!")
-def sort_vcf(filename):
-    new_filename = filename[:-4]+'.sort.vcf'
-    cmd_s1='chr_order="chrMnchr1nchr2nchr3nchr4nchr5nchr6nchr7nchr8nchr9nchrUn"'
-    os.system(cmd_s1)
-    cmd_s2='cat {} | grep "^#" > header.vcf'.format(filename)
-    os.system(cmd_s2)
-    cmd_s3='cat {} | grep -v "^#" | sort -k1,1 -k2,2n > pre.sorted.vcf'.format(filename)
-    os.system(cmd_s3)
-    cmd_s4='cat header.vcf pre.sorted.vcf >{}'.format(new_filename)
-    os.system(cmd_s4)
-##run step3
-sort_vcf(input[:-4]+'.normal.vcf')
 
 
-print("reduce start!!")
-def reduce_vcf(filename):
-    readDir = filename
-    writeDir = filename[:-4]+'.reduce.vcf'
-    outfile=open(writeDir,"w")
-    f = open(readDir,"r")
-    pos_seen = set()  # Build an unordered collection of unique elements.
-    for line in f:
-        line = line.strip('\n')
-        line2=line.split('\t')
-        if len(line2)==1:
-            outfile.write(line +'\n')
-        if len(line2)>1:
-            pos = line2[1]
-            if pos not in pos_seen:
-                outfile.write(line+ '\n')
-                pos_seen.add(pos)               
-##run step4
-reduce_vcf(input[:-4]+'.normal.sort.vcf')
-
-print("check start!!")
 def check_vcf(filename,genome):
+    print("check start!!")
     cmd_0 = 'vg construct -r {} -v {} 1>test.vg 2>error'.format(genome,filename)
     os.system(cmd_0)
     size = os.path.getsize('error')
     if size == 0:
         print('Susceed!')
     else:
-        print('Faild')
-##run step5 check
-check_vcf(input[:-4]+'.normal.sort.reduce.vcf',genome)
+        print('Faild','try again!')
+        sort_vcf(filename)
+        reduce_vcf(filename)
+        clear_vcf(filename,genome)
+        test_again_filename=filename+'.sort.reduce.vcf'
+        cmd_again = 'vg construct -r {} -v {} 1>test.vg 2>error'.format(genome,filename)
+        os.system(cmd_again)
+        size = os.path.getsize('error')
+        if size == 0:
+            print('Susceed!')
+        else:
+            print('Faild','Fetal error!')
 
 
+if args.process=='all':
+    ##run step1
+    ngmlr_vcf_2_normal_vcf(input)
+    ##run step2
+    sort_vcf(input[:-4]+'.normal.vcf')
+    ##run step3
+    reduce_vcf(input[:-4]+'.normal.sort.vcf')
+    ##run step4
+    clear_vcf(input[:-4]+'.normal.sort.reduce.vcf',genome)
+    ##run step5 check
+    check_vcf(input[:-4]+'.normal.sort.reduce.vcf',genome)
+
+if args.process=='normal':
+    ##run step1
+    ngmlr_vcf_2_normal_vcf(input)
+
+if args.process=='sort':
+    ##run step2
+    sort_vcf(input[:-4]+'.vcf')
+    ##run step3
+    reduce_vcf(input[:-4]+'.sort.vcf')
+
+if args.process=='clear':
+    ##run step4
+    clear_vcf(input[:-4]+'.vcf',genome)
+    ##run step5 check
+    check_vcf(input[:-4]+'.vcf',genome)
